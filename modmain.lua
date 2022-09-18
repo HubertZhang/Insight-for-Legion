3523e27421c7f60fe83ef6a49822448b5fc679bb
Hubert Zhang <hubert.zyk@gmail.com>
2022-09-18 18:32:21 +0800

    Modify initialization of mod

--- modmain.lua
+++ modmain.lua
@@ -2,6 +2,8 @@ GLOBAL.setmetatable(env, {
     __index = function(t, k) return GLOBAL.rawget(GLOBAL, k) end
 })
 
+local _G = GLOBAL
+
 local function SetPrefabDescriptor(name, module)
     module = module or name
     local res = require("prefab_descriptors/" .. module)
@@ -15,7 +17,7 @@ local function SetPrefabDescriptor(name, module)
         setmetatable(res, {})
     end
 
-    rawset(GLOBAL.Insight.prefab_descriptors, name, res)
+    rawset(_G.Insight.prefab_descriptors, name, res)
 end
 
 local function SetComponentsDescriptor(name, module)
@@ -31,13 +33,16 @@ local function SetComponentsDescriptor(name, module)
         setmetatable(res, {})
     end
 
-    rawset(GLOBAL.Insight.descriptors, name, res)
+    rawset(_G.Insight.descriptors, name, res)
 end
 
-print("Insight enabled: ", KnownModIndex:IsModEnabled("workshop-2189004162"))
+local function AddDescriptors()
+    print("[Insight-Legion]: Insight enabled: ", KnownModIndex:IsModEnabled("workshop-2189004162"))
+    if not KnownModIndex:IsModEnabled("workshop-2189004162") then return end
+    print("[Insight-Legion]: Legion enabled: ", KnownModIndex:IsModEnabled("workshop-1392778117"))
+    if not KnownModIndex:IsModEnabled("workshop-1392778117") then return end
 
-if KnownModIndex:IsModEnabled("workshop-2189004162") then
-    print("Add prefab descriptors")
+    print("[Insight-Legion]: Add prefab descriptors")
     local prefabs = {
         "siving_ctlwater",
         "siving_ctldirt",
@@ -46,13 +51,16 @@ if KnownModIndex:IsModEnabled("workshop-2189004162") then
 
         "tourmalinecore",
         "moondungeon",
-        "web_hump" }
+        "web_hump"
+    }
     for index, value in pairs(prefabs) do SetPrefabDescriptor(value) end
 
-    print("Add components descriptors")
+    print("[Insight-Legion]: Add components descriptors")
     SetComponentsDescriptor("perennialcrop")
     SetComponentsDescriptor("perennialcrop2")
     SetComponentsDescriptor("fishhomingbait")
 
-    rawset(GLOBAL.Insight.prefab_descriptors, "icire_rock", GLOBAL.Insight.prefab_descriptors["heatrock"])
+    rawset(_G.Insight.prefab_descriptors, "icire_rock", _G.Insight.prefab_descriptors["heatrock"])
 end
+
+AddSimPostInit(AddDescriptors)
